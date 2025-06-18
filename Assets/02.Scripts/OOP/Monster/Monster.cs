6a5925ece917d526e7753e29f7e175190ad707b1
Sooezfor <sangkyu3395@naver.com>
2025-06-18 11:51:41 +0900

    0618class1

--- Assets/02.Scripts/OOP/Monster/Monster.cs
+++ Assets/02.Scripts/OOP/Monster/Monster.cs
@@ -1,10 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public abstract class Monster : MonoBehaviour
 {
-    public SpriteRenderer sRenderer;
+    SpriteRenderer sRenderer;
     [SerializeField] protected float Hp = 3f;
     [SerializeField] protected float moveSpeed = 3f;
+    Animator anim;
+    bool isMove = true; //공격당했을때 멈추게하기 위해 움직이는거 판단하는 불값
+    bool isHit = false; //Hit 함수가 돌고있을 때 그다음 공격은 막도록 하는 것(광클방지)
 
     private int dir = 1;
 
@@ -13,6 +17,7 @@ public abstract class Monster : MonoBehaviour
     private void Start()
     {
         sRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
 
         Init();
     }
@@ -23,30 +28,49 @@ public abstract class Monster : MonoBehaviour
 
     private void OnMouseDown()//마우스 클릭 시 반응하는 함수,유니티 기본 함수
     {
-        Hit(1); //데미지 1
+        StartCoroutine(Hit(1)); //데미지 1
     }
-    private void Hit(float damage) //그냥 게임뷰 상에서 마우스로 클릭하면 상호작용됨
+    IEnumerator Hit(float damage) //그냥 게임뷰 상에서 마우스로 클릭하면 상호작용됨
     {
-        Hp -= damage;
-        if(Hp <= 0)
-        {
-            Debug.Log("몬스터 죽음");
-            Destroy(gameObject);
-        }
+        if (isHit)
+            yield break; //isHit이 true면 현재 코루틴 실행X 현재루틴 파괴
+
+            isHit = true;
+            isMove = false; 
+            anim.SetTrigger("Hit");
+
+            Hp -= damage;
+
+            if(Hp <= 0)
+            {
+                anim.SetTrigger("Death");
+
+                yield return new WaitForSeconds(3f);
+                Destroy(gameObject);
+
+                yield break; 
+            }
+            yield return new WaitForSeconds(0.5f);
+            isMove = true;
+            isHit = false;
     }
     private void Move()
     {
-        transform.position += Vector3.right * dir * moveSpeed * Time.deltaTime;
+        if (!isMove) 
+            return; 
 
-        if (transform.position.x > 8f)
-        {
-            dir = -1; //방향 반대로
-            sRenderer.flipX = true;
-        }
-        else if (transform.position.x < -8f)
-        {
-            dir = 1;
-            sRenderer.flipX = false;
-        }
+            transform.position += Vector3.right * dir * moveSpeed * Time.deltaTime;
+
+            if (transform.position.x > 8f)
+            {
+                dir = -1; //방향 반대로
+                sRenderer.flipX = true;
+            }
+            else if (transform.position.x < -8f)
+            {
+                dir = 1;
+                sRenderer.flipX = false;
+            }
     }
 }
+
